6e44079006acb640731d9001430d7fb20eb198f0
UnamedRus <UnamedRus@users.noreply.github.com>
2022-01-13 01:06:05 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -15,5 +15,25 @@ CREATE TABLE IF NOT EXISTS logs.logs_local ON CLUSTER '{cluster}' (
   TTL toDateTime(timestamp) + INTERVAL 30 DAY DELETE
   PARTITION BY toDate(timestamp)
   ORDER BY (cluster, namespace, app, pod_name, container_name, host, -toUnixTimestamp(timestamp));
+  
+CREATE TABLE IF NOT EXISTS logs.logs_local ON CLUSTER `{cluster}`
+(
+    `timestamp` DateTime64(3) CODEC(Delta, LZ4),
+    `cluster` LowCardinality(String),
+    `namespace` LowCardinality(String),
+    `app` LowCardinality(String),
+    `pod_name` LowCardinality(String),
+    `container_name` LowCardinality(String),
+    `host` LowCardinality(String),
+    `fields_string.key` Array(LowCardinality(String)),
+    `fields_string.value` Array(String) CODEC(ZSTD(1)),
+    `fields_number.key` Array(LowCardinality(String)),
+    `fields_number.value` Array(Float64),
+    `log` String CODEC(ZSTD(1))
+)
+ENGINE = ReplicatedMergeTree
+PARTITION BY toDate(timestamp)
+ORDER BY (cluster, namespace, app, pod_name, container_name, host, timestamp)
+TTL toDateTime(timestamp) + INTERVAL 30 DAY
 
 CREATE TABLE IF NOT EXISTS logs.logs ON CLUSTER '{cluster}' AS logs.logs_local ENGINE = Distributed('{cluster}', logs, logs_local, rand());
